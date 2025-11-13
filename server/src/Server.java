76911c47306c9a115d3f771cc9a7a7e33d3e66c3
Erbol Zhaparov <ae.270718@gmail.com>
2025-11-13 19:49:14 +0600

    add message reversing feature

--- server/src/Server.java
+++ server/src/Server.java
@@ -5,6 +5,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 public class Server {
     private final int port;
@@ -33,12 +35,21 @@ public class Server {
     private void handle(Socket clientSocket) throws IOException {
         InputStream inputStream = clientSocket.getInputStream();
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        OutputStream output = clientSocket.getOutputStream();
+        PrintWriter printWriter = new PrintWriter(output);
 
         try (Scanner scanner = new Scanner(inputStreamReader)) {
             while (true) {
                 String message = scanner.nextLine().trim();
                 System.out.printf("Got message: %s%n", message);
 
+                StringBuilder builder = new StringBuilder();
+                String reversedMessage = builder.append(message).reverse().toString();
+                printWriter.print(reversedMessage);
+                printWriter.write(System.lineSeparator());
+                printWriter.flush();
+
+                System.out.print("Reversing message...");
                 if (message.equalsIgnoreCase("stop")) {
                     System.out.println("Stopping server...");
                     return;
