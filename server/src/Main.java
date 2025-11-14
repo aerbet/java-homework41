0bdd36ce505583b90386da3230b5792313e9d506
Erbol Zhaparov <123570177+aerbet@users.noreply.github.com>
2025-11-14 18:13:07 +0600

    update Main.java, delete comments

--- server/src/Main.java
+++ server/src/Main.java
@@ -2,12 +2,6 @@
 
 public class Main {
     public static void main(String[] args) {
-
-//        Server server = new Server(8089);
-//        server.run();
-
-        // паттерн фабрика
         Server.bindToServer(8089).run();
-
     }
 }
